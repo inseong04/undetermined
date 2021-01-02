52d7e237d3864685b27410dd689d57ffc295c051
ois0531 <ois0531@naver.com>
2021-01-02 23:08:11 +0900

    Jsoup

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,58 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Bundle bundle = new Bundle();
+        TextView person_t = findViewById(R.id.person);
+
+            new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        Document document = null;
+                        document = Jsoup.connect("https://www.bloodinfo.net/bloodstats_statistics.do").get();
+                        Elements elements = document.select("#area_blood_stock_numbers");
+                        String person = elements.select(".txt").toString();
+                        Log.e("test","person : "+person);
+                        bundle.putString("person",person);
+                        Message msg = handler.obtainMessage();
+                        msg.setData(bundle);
+                        handler.sendMessage(msg);
+/*                        Document document = null;
+                        document = (Document) Jsoup.connect("https://www.bloodinfo.net/bloodstats_statistics.do").get();
+                        Elements elements = document.select("#txt");*/
+                    }catch(IOException e){
+                        Log.e("error","error ouccured");
+                    }
+                }
+            }.start();
+
+
     }
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            TextView person_t = findViewById(R.id.person);
+            Bundle bundle = msg.getData();
+            person_t.setText(bundle.getString("person"));
+        }
+    };
 }
--- app/src/main/java/com/example/myapplication/signup/Intro.java
+++ app/src/main/java/com/example/myapplication/signup/Intro.java
@@ -1,30 +1,45 @@
 package com.example.myapplication.signup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.myapplication.Login;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 public class Intro extends AppCompatActivity {
 
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        SharedPreferences sharedPreferences = getSharedPreferences("Login_information",0);
 
-        // TODO : 인트로에서 자동로그인을 체크챘는지 안했는지 확인하여 if문 사용해 Login 또는 Mainactivity로 이동 코드짜야함.
+        splash();
 
+    }
+
+    protected void splash(){
         TextView tv1 = findViewById(R.id.tv1);
         TextView tv2 = findViewById(R.id.tv2);
         TextView logo = findViewById(R.id.logo);
 
-        Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
                 tv1.setVisibility(View.VISIBLE);
@@ -48,4 +63,5 @@ public class Intro extends AppCompatActivity {
             }
         }, 3500);
     }
+
 }
